2546bad877588823b46e619301d290bc7bc7f7d3
Nikolay Shein <qseboy1998@gmail.com>
2023-05-26 17:14:21 +0300

    1 tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+const galleryEl = document.querySelector('.gallery');
+
+// render gallery
+const list = galleryItems
+  .map(
+    el =>
+      `<li class="gallery__item">
+      <a href="${el.original}" class="gallery__link">
+        <img class="gallery__image" src="${el.preview}" alt="${el.description}" data-source="${el.original}">
+      </a>
+    </li>`
+  )
+  .join('');
+galleryEl.insertAdjacentHTML('afterbegin', list);
+
+// handle click
+const handleClickToImage = event => {
+  event.preventDefault();
+  const itemOriginalSize = event.target.dataset.source;
+  const handleEsc = event => {
+    if (event.code === 'Escape') {
+      console.log('esc');
+      instance.close();
+    }
+  };
+
+  const instance = basicLightbox.create(
+    `
+    <div class="modal">
+        <img src="${itemOriginalSize}">
+    </div>
+    `,
+    {
+      closable: true,
+      onClose: () => {
+        galleryEl.removeEventListener('keydown', handleEsc);
+      },
+    }
+  );
+
+  instance.show();
+  galleryEl.addEventListener('keydown', handleEsc);
+};
+
+galleryEl.addEventListener('click', handleClickToImage);
